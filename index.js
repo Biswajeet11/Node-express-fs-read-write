ba67c6600a28b5dd6c5c29af84608325a4f4b8d2
Biswajeet11 <biswajeetgp25@gmail.com>
2019-09-08 20:18:11 +0530

    put request added for student json

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const jsonFile = 'student.json';
-const encode = 'utf-8'
+const encode = 'utf-8';
 const PORT = 3010;
-app.use(express.json())
+app.use(express.json());
 
 app.get('/students', function (req, res) {
     fs.readFile(jsonFile, encode, function (err, student) {
@@ -21,7 +21,7 @@ app.post('/students', function (req, res) {
         else {
             const student = req.body
             const _id = Date.now()
-            Object.assign(student, _id)
+            Object.assign(student, { _id })
             students.push(student)
             fs.writeFile(jsonFile, JSON.stringify(students), function (err) {
                 if (err) {
@@ -29,6 +29,34 @@ app.post('/students', function (req, res) {
                 }
                 res.json({ notice: 'Post has been saved', students })
             });
+        }
+    })
+})
+
+app.put('/students/:id', function (req, res) {
+    const id = req.params.id
+    const student = req.body
+    fs.readFile(jsonFile, encode, function (err, data) {
+        if (err) {
+            res.json(err)
+        }
+        else {
+            const students = [JSON.parse(data)]
+            console.log(students)
+            const studentData = students.find((student) => {
+                return student._id === Number(id)
+            })
+            if (studentData) {
+                Object.assign(studentData, student)
+                fs.writeFile(jsonFile, JSON.stringify(studentData), function (err) {
+                    if (err) {
+                        res.json({ err })
+                    }
+                    else {
+                        res.json({ notice: 'Data has been updated', studentData })
+                    }
+                })
+            }
         }
     })
 })
